c18b23ddc0da4ae6e537bae493c26f640907b35c
Jibone <jibone@gmail.com>
2024-03-28 11:39:23 +0800

    Refactor: tukar design komponen KadIklan

--- web/src/ui/branding/CountryPill.tsx
+++ web/src/ui/branding/CountryPill.tsx
@@ -10,11 +10,11 @@ export default function CountryPill({ country }: { country: CountryList }) {
   };
 
   return (
-    <div data-testid="country-pill-container" className="text-xs">
-      <span className="text-base">{flagEmoji[country]}</span>
-      <span className="hidden md:inline-block">
-        {country.replace(/\b\w/g, (char) => char.toUpperCase())}
-      </span>
+    <div
+      data-testid="country-pill-container"
+      className="text-xs group-hover:text-2xl transition-all duration-300 px-1 py-1 group-hover:px-2 bg-white border border-black rounded-full shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]"
+    >
+      <span className="">{flagEmoji[country]}</span>
     </div>
   );
 }
--- web/src/ui/koleksi/KadIklan.tsx
+++ web/src/ui/koleksi/KadIklan.tsx
@@ -1,7 +1,6 @@
 import type { Iklan } from "@/db/schema/iklan";
 import Link from "next/link";
 import { YTThumbnail } from "@/ui/thumbnail";
-import { YearColorPill } from "@/ui/branding";
 import { CountryPill } from "@/ui/branding";
 
 type KadIklanProps = {
@@ -13,7 +12,7 @@ export default function KadIklan({ iklan, pill }: KadIklanProps) {
   return (
     <Link
       href={`/${iklan.slug}`}
-      className="flex flex-wrap p-2 rounded-xl overflow-clip bg-white border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] transition-all duration-300 hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] active:shadow-[0px_0px_0px_0px_rgba(0,0,0,1)]"
+      className="group flex flex-wrap p-2 rounded-xl overflow-ellipsis bg-white border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] transition-all duration-300 hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] active:shadow-[0px_0px_0px_0px_rgba(0,0,0,1)]"
     >
       <div className="w-2/5 h-fit aspect-auto rounded-xl border-2 border-black overflow-clip">
         <YTThumbnail vidId={iklan.id} altText={iklan.title} />
@@ -21,15 +20,8 @@ export default function KadIklan({ iklan, pill }: KadIklanProps) {
       <div className="w-3/5 px-2 flex flex-col justify-between">
         <div className="font-semibold text-base lg:text-lg">{iklan.title}</div>
         <div className="relative">
-          <div className="w-3/5 absolute bottom-0 left-0">
-            {iklan.organization}
-          </div>
-          <div className="absolute bottom-0 right-0">
-            {pill === "year" ? (
-              <YearColorPill yearStr={iklan.year} />
-            ) : (
-              <CountryPill country={iklan.country} />
-            )}
+          <div className="absolute z-50 -bottom-5 -right-7">
+            <CountryPill country={iklan.country} />
           </div>
         </div>
       </div>
