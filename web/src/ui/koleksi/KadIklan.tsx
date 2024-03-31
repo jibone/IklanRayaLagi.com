fa5a6730c1a405906267d4ae7e1e40283d652f64
Jibone <jibone@gmail.com>
2024-03-31 18:46:42 +0800

    Refactor: ubah design kad iklan kasi cantik sikit

--- web/src/ui/koleksi/KadIklan.tsx
+++ web/src/ui/koleksi/KadIklan.tsx
@@ -11,15 +11,16 @@ export default function KadIklan({ iklan }: KadIklanProps) {
   return (
     <Link
       href={`/${iklan.slug}`}
-      className="group flex flex-wrap p-2 rounded-xl overflow-ellipsis bg-white border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] transition-all duration-300 hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] active:shadow-[0px_0px_0px_0px_rgba(0,0,0,1)]"
+      className="group flex flex-col relative p-2 rounded-xl overflow-ellipsis bg-white border-2 border-black shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] transition-all duration-300 hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] active:shadow-[0px_0px_0px_0px_rgba(0,0,0,1)]"
     >
-      <div className="w-2/5 h-fit aspect-auto rounded-xl border-2 border-black overflow-clip">
+      <div className="w-full h-fit aspect-auto rounded-xl border-2 border-black overflow-clip">
         <YTThumbnail vidId={iklan.id} altText={iklan.title} />
       </div>
-      <div className="w-3/5 px-2 flex flex-col justify-between">
-        <div className="font-semibold text-base lg:text-lg">{iklan.title}</div>
-        <div className="relative">
-          <div className="absolute z-50 -bottom-5 -right-7">
+      <div className="w-full px-2 py-2 flex flex-col justify-between">
+        <div className="font-semibold text-lg lg:text-xl">{iklan.title}</div>
+        <div className="h-full flex justify-between">
+          <div className="w-fit">{iklan.organization}</div>
+          <div className="w-fit absolute -bottom-3 -right-3">
             <PilNegara negara={iklan.country} />
           </div>
         </div>
