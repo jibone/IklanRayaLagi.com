8fcb11aa09ec31f231742855c8b6a0ac82388588
Jibone <jibone@gmail.com>
2024-04-16 12:53:26 +0800

    Chore: Housekeeping code cleanup

--- web/src/app/bilik_media/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import { OGBilikMedia } from "@/ui/ogimage";
-
-export const runtime = "edge";
-export const alt = "Bilik Media Iklan Raya Lagi";
-export const contentType = "image/png";
-
-export default function OpenGraphImage() {
-  return OGBilikMedia();
-}
